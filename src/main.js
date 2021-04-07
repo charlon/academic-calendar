1cdbe74a75f802082bf2323e5f9c93cf3b0cd387
Charlon Palacay <charlon@gmail.com>
2021-04-07 12:25:25 -0700

    Removes tailwindcss from build.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,4 @@ import router from './router'
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap";
 
-// tailwind
-// import './index.css'
-
 createApp(App).use(router).mount('#app')
