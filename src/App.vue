4352fe35eea0d10fc6c91849e8a22ac88b953c8a
Charlon Palacay <charlon@gmail.com>
2021-04-07 13:43:43 -0700

    Adds events-table component. Removes extra about page and component.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
 
-
-  <h1>This is Vue!</h1>
-
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-
     <router-view />
 
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
   },
 };
 </script>
