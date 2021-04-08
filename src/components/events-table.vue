eeba5167a8634cc2e5c3f6390df6fe82d374188d
Charlon Palacay <charlon@gmail.com>
2021-04-08 15:58:26 -0700

    Fixes Bootstrap inclusion and removes table striping jquery.

--- src/components/events-table.vue
+++ src/components/events-table.vue
@@ -1,7 +1,7 @@
 <template>
-  <h2 style="margin:0;">Event Category 1</h2>
-  <p style="margin:0;">This is a special message.</p>
-  <table class="table table-condensed" style="margin-top:2rem; margin-bottom:0;">
+  <h2 style="margin:0;">Event Category xx</h2>
+  <p style="margin:0;">This is a category note</p>
+  <table class="table table-sm table-dark" style="margin-top:2rem; margin-bottom:0;">
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -31,7 +31,6 @@
     </tr>
   </tbody>
 </table>
-<p class="text-muted small">Note: the page has a script that appends table-striped class.</p>  
 </template>
 
 
