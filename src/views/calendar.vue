aa3881b1f886a10b3607671bb3f351668899b190
Charlon Palacay <charlon@gmail.com>
2021-04-12 17:27:55 -0700

    Update json and render table component per category.

--- src/views/calendar.vue
+++ src/views/calendar.vue
@@ -1,30 +1,33 @@
 <template>
   <div style="outline: dashed 2px #ccc">
-   
-    <h1 class="mb-4"><span class="text-danger">{{ calendar.years }}</span> Academic Calendar</h1>
+    <h1 class="mb-4">
+      <span class="text-danger">{{ calendar.range }}</span> Academic Calendar
+    </h1>
 
     <h2>Contents</h2>
     <ul>
-      <li v-for="category in categories" v-bind:key="category.category" class="text-danger">{{ category.category }}</li>
+      <li
+        v-for="category in categories"
+        v-bind:key="category.label"
+        class="text-danger"
+      >
+        {{ category.label }}
+      </li>
     </ul>
-
-    <p>See UW Online Learning for drop and refund information associated with
-      Online Learning courses offered by UW Professional &amp; Continuing Education.
+    <p>
+      See UW Online Learning for drop and refund information associated with
+      Online Learning courses offered by UW Professional &amp; Continuing
+      Education.
     </p>
 
-    <h2>Event Cateogry 1</h2>
-    <p>This is a category note</p>
-    
-    <events-table />
-
-
-    <h2 class="mt-5">Event Category 2</h2>
-    <p>This is a category note</p>
-    <events-table />
-
+    <template v-for="category in categories" v-bind:key="category.label">
+      <h2 class="mt-5 text-danger">{{ category.label }}</h2>
+      <p v-if="category.note" class="text-danger">{{ category.note }}</p>
+      <calendar-category />
+    </template>
   </div>
 
-  <h2>Holidays and Observances</h2>
+  <h2 class="mt-5">Holidays and Observances</h2>
   <p>
     For Washington’s holiday and observance calendar, please review the
     Washington State Council of Presidents
@@ -38,27 +41,35 @@
 
 <script>
 import Calendars from "../assets/calendars.json";
-import EventsTable from "../components/events-table.vue";
+import CalendarCategory from "../components/calendar-category.vue";
 
 export default {
   name: "App",
   data() {
     return {
-      calendar : { "years": "2020-2021" },
+      calendar: { range: "2020-2021" },
       categories: [
-        { "category": "Calendar Summary"},
-        { "category": "Dates of Instruction"},
-        { "category": "University Holidays"},
-        { "category": "Application Deadlines"},
-        { "category": "Registration Deadlines"},
-        { "category": "Adding/Dropping Courses or Withdrawl"},
-        { "category": "Tuition/Fee Assessment"},
-        { "category": "Grade Deadlines"},
-      ]
-    }
+        { label: "Calendar Summary" },
+        { label: "Dates of Instruction" },
+        {
+          label: "University Holidays",
+          note:
+            "Classes are not in session on the following holidays. All offices and most University buildings are also closed.",
+        },
+        { label: "Application Deadlines" },
+        { label: "Registration Deadlines" },
+        { label: "Adding/Dropping Courses or Withdrawl" },
+        { label: "Tuition/Fee Assessment" },
+        {
+          label: "Grade Deadlines",
+          note:
+            "Autumn registration for students dropped Spring 2020 will be canceled on Aug 21, 2020.",
+        },
+      ],
+    };
   },
   components: {
-    "events-table": EventsTable,
+    "calendar-category": CalendarCategory,
   },
 };
 </script>
@@ -66,7 +77,6 @@ export default {
 <style lang="scss" scoped>
 @import "bootstrap/dist/css/bootstrap.css";
 
-// bootrap and uw.css overrides
 h1 {
   font-size: 3.7rem; // 37px
   font-weight: bold;
@@ -76,5 +86,4 @@ h2 {
   font-size: 2.8rem;
   font-weight: bold;
 }
-
 </style>
