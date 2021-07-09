0df9048f39aaaa988ce26e7fb2af4f8eb30a3e2e
Charlon Palacay <charlon@gmail.com>
2021-07-09 13:18:50 -0700

    Update to Bootstrap 5 for vite apps.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' 
 
-// bootstrap 4 (standalone)
-//import "bootstrap/dist/css/bootstrap.min.css";
-//import "bootstrap";
+// bootstrap 5 (standalone)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 createApp(App).use(router).mount('#app')
